dc780686b6a8f178100c733aaa07fe9b414105ea
Grandhawa12 <Grandhawa12@studentmail.nait.ca>
2021-01-13 10:15:56 -0700

    create first sample console program

---
+++ FirstSampleSolution/FirstSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The WriteLine() command will output the line to the console window.
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This course section is: \t OA03");
+            Console.WriteLine("Your instructors name is: \t Don Welch");
+
+        }
+    }
+}
---
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is how you make a sandwich:");
+            Console.WriteLine("First get a slice of bread");
+            Console.WriteLine("Put some butter onto a butter knife and spread the butter onto the bread");
+            Console.WriteLine("Add condiments such as kethup or mustard to the bread");
+            Console.WriteLine("Place some tomato slices onto the bread");
+            Console.WriteLine("Add a slice of any meat your prefer on top of the tomatos");
+            Console.WriteLine("Add some lettece on top of the meat");
+            Console.WriteLine("Place the second slice of bread on top to finish the sandwich");
+
+        }
+    }
+}
